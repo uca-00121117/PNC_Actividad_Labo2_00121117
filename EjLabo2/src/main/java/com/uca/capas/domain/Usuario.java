c0886ea1b91e8befc7ff2b4141dfb318cea3ffdd
Alejandro Rubi <00121117@uca.edu.sv>
2020-04-01 18:59:16 -1200

    first commit

---
+++ EjLabo2/src/main/java/com/uca/capas/domain/Usuario.java
@@ -0,0 +1,22 @@
+package com.uca.capas.domain;
+
+public class Usuario {
+	private String nombre;
+    private String occupation;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getOcupacion() {
+        return occupation;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.occupation = ocupacion;
+    }
+}
